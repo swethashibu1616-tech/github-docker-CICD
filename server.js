e54e3fbc1ad8e812a4bc895019d0b55ae1613f97
swetha Kumar <sweladdu1615@gmail.com>
2026-05-19 22:24:30 +0530

    inital docker commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("SWETHA SHIBU GITHUB DOCKER CI/CD Running Sucessfully");
+});
+
+app.get("/health", (req, res) => {
+  res.send("Healthy");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
